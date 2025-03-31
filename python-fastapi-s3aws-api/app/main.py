2b39bb61d37378a8dc99e156e53764cd8322f599
dsabau <danioansabau@icloud.com>
2025-04-01 01:29:34 +0300

    Improved security & added Docker files.

--- python-fastapi-s3aws-api/app/main.py
+++ python-fastapi-s3aws-api/app/main.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes.qr import router as qr_router
-from configs.constants import DEFAULT_LOG_MESSAGE_FORMAT, ALLOWED_ORIGINS
+from configs.constants import (
+    DEFAULT_LOG_MESSAGE_FORMAT,
+    ALLOWED_ORIGINS
+)
 
 
 logging.basicConfig(level=logging.INFO, format=DEFAULT_LOG_MESSAGE_FORMAT)
@@ -14,8 +17,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["POST"],
+    allow_headers=["*"]
 )
 
 app.include_router(qr_router)
